904ad796363a4e63dc32f16ffa41aa7f6c9fcc18
nbur4556 <nbur4556@gmail.com>
2021-03-30 17:03:56 -0500

    bulk delete todo query

--- src/components/Todo/TodoPrivateList.js
+++ src/components/Todo/TodoPrivateList.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useMutation, gql } from "@apollo/client";
 
 import TodoItem from "./TodoItem";
 import TodoFilters from "./TodoFilters";
@@ -18,6 +18,16 @@ const GET_MY_TODOS = gql`
   }
 `;
 
+const CLEAR_COMPLETED = gql`
+  mutation clearCompleted {
+    delete_todos(
+      where: { is_completed: { _eq: true }, is_public: { _eq: false } }
+    ) {
+      affected_rows
+    }
+  }
+`;
+
 const TodoPrivateList = props => {
   const [state, setState] = useState({
     filter: "all",
@@ -31,7 +41,18 @@ const TodoPrivateList = props => {
     });
   };
 
-  const clearCompleted = () => { };
+  const [clearCompletedTodos] = useMutation(CLEAR_COMPLETED);
+
+  const clearCompleted = () => {
+    clearCompletedTodos({
+      optimisticResponse: true,
+      update: (cache, { data }) => {
+        const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+        const newTodos = existingTodos.todos.filter(t => !t.is_completed);
+        cache.writeQuery({ query: GET_MY_TODOS, data: { todos: newTodos } });
+      }
+    })
+  };
 
   const { todos } = props;
   let filteredTodos = todos;
